9fd65a09c7efcbacc9e0342fe5e608452329eaf1
Don-ovan <82923038+Don-ovan@users.noreply.github.com>
2021-04-23 19:27:03 -0400

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,13 @@
+#Tests Binary, Octal and Hexadecimal values and print the results to the screen.
+Test = 0b100
+print("100 Binary: ", Test)
+
+Test = 0o100
+print("100 Octal", Test)
+
+Test = 0x100
+print("100 Hexadecimal: ", Test)
+
+print(bin(Test))
+print(oct(Test))
+print(hex(Test))
